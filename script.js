52e869cb97a9491a9d0ade98eaf7a2ec19ea39af
Ryder Dale <rydersdale@gmail.com>
2018-05-18 15:21:20 -0500

    checkerboard

--- script.js
+++ script.js
@@ -5,19 +5,27 @@ document.body.setAttribute("style", "background: solid red");
 (function() {
     
     //document.body.style.backgroundColor = "red";
-    for (let i=0; i<8; i++) {
-        console.log("affirmative");
-        let checkerBoardSquare = document.createElement('div');
-        document.body.appendChild(checkerBoardSquare);
-        
-        
-        // console.log(checkerBoardSquare);
-        checkerBoardSquare.style.width ="11.1%";
-        checkerBoardSquare.style.float="left";
-        checkerBoardSquare.style.paddingBottom="11.11%";
-        checkerBoardSquare.style.backgroundColor='black';
-    }
+ 
+    function makeCheckerBoardRow(rowNumber) {
+        for (let i=0; i<9; i++) {
+            console.log("affirmative");
+            let checkerBoardSquare = document.createElement('div');
+            document.body.appendChild(checkerBoardSquare);
+            checkerBoardSquare.style.width ="11.1%";
+            checkerBoardSquare.style.float="left";
+            checkerBoardSquare.style.paddingBottom="11.11%";
+            if ((i+rowNumber)%2 === 0){
+                checkerBoardSquare.style.backgroundColor='black';
+            }
+            else {
+                checkerBoardSquare.style.backgroundColor='red';
+            };
 
+        }   
+    };
+    for (let i=0; i<9; i++) {
+        makeCheckerBoardRow(i);
+    };
     
 })();
 
