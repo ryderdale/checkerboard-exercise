ac609266e927aa994c1070226c44f5de87c32d9c
Ryder Dale <rydersdale@gmail.com>
2018-05-18 14:41:40 -0500

    first row of chckerboard squares appearing

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your JS goes here
+
+document.body.setAttribute("style", "background: solid red");
+
+(function() {
+    
+    //document.body.style.backgroundColor = "red";
+    for (let i=0; i<8; i++) {
+        console.log("affirmative");
+        let checkerBoardSquare = document.createElement('div');
+        document.body.appendChild(checkerBoardSquare);
+        
+        
+        // console.log(checkerBoardSquare);
+        checkerBoardSquare.style.width ="11.1%";
+        checkerBoardSquare.style.float="left";
+        checkerBoardSquare.style.paddingBottom="11.11%";
+        checkerBoardSquare.style.backgroundColor='black';
+    }
+
+    
+})();
+
+
